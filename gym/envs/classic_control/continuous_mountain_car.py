b0a1718fc363f048258f4b29f0cd2896262a6ccb
Amrita A. Nair <68496782+AmritaANair@users.noreply.github.com>
2023-06-19 11:56:46 +0530

    more wheel changes

--- gym/envs/classic_control/continuous_mountain_car.py
+++ gym/envs/classic_control/continuous_mountain_car.py
@@ -260,9 +260,14 @@ class Continuous_MountainCarEnv(gym.Env):
         for c in [(carwidth / 2, 0), (-carwidth / 2, 0)]:
             c = pygame.math.Vector2(c).rotate_rad(math.cos(3 * pos))
             wheel = (
-                int(c[0] + (pos - self.min_position) * scale),
+                int(c[0] + (pos) * scale),
                 int(c[1] + clearance + self._height(pos) * scale),
             )
+            '''wheel = (
+                int(c[0] + (pos - self.min_position) * scale),
+                int(c[1] + clearance + self._height(pos) * scale),
+            )'''
+        
 
             gfxdraw.aacircle(
                 self.surf, wheel[0], wheel[1], int(carheight / 2.5), (128, 128, 128)
