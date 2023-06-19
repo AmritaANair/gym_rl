a62f8e1994af8efbd9e61ae0625ebcff0d5e010c
Amrita A. Nair <68496782+AmritaANair@users.noreply.github.com>
2023-06-19 19:44:16 +0530

    actually back to old car

--- gym/envs/classic_control/continuous_mountain_car.py
+++ gym/envs/classic_control/continuous_mountain_car.py
@@ -260,7 +260,7 @@ class Continuous_MountainCarEnv(gym.Env):
         for c in [(carwidth / 4, 0), (-carwidth / 4, 0)]:
             c = pygame.math.Vector2(c).rotate_rad(math.cos(3 * pos))
             wheel = (
-                int(c[0] + (pos) * scale),
+                int(c[0] + (pos - self.min_position) * scale),
                 int(c[1] + clearance + self._height(pos) * scale),
             )
 
